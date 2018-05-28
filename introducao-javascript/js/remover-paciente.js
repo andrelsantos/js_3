51afc2c9ad4345193b3f9e3a1da49935c12a31fc
andrelsantos <andre.lsantos.info@gmail.com>
2018-05-27 22:35:03 -0300

    Curso concluido

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -1,8 +1,9 @@
-console.log("Carregado de arquivo externo 2");
-var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
 
-pacientes.forEach(function(paciente) {
-  paciente.addEventListener("dblclick", function() {
-    this.remove();
-  });
+tabela.addEventListener("dblclick",function(event){
+  event.target.parentNode.classList.add("fadeOut");
+  setTimeout (function(){
+      event.target.parentNode.remove();
+  },500)
+
 });
